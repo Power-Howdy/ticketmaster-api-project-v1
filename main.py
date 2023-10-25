57e6d68aa32e77be217799f40ec2fb3c70eb253d
Mad Devil Coder <giftedclan0305@gmail.com>
2023-10-25 11:24:54 -0500

    fixed all the bugs

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from ticket_master import TicketMasterAPI
-from google_sheet_manager import GoogleSheetManager
-# get data
+# ticket master data
 base_url = "https://app.ticketmaster.com/discovery/v2/events.json"
 api_key = "Skl0KMwYVc7suTtyYkxqUJPR1S37dtB2"
-
-tm_api = TicketMasterAPI(base_url, api_key)
-all_data = tm_api.get_all_data()
-
-
-# insert data
+# google sheet manager data
 credentials_file = 'fleet-joy-392518-26ce6cb44572.json'
 sheet_name = 'Ticket master sheet'
 
-# Initialize the GoogleSheetManager object
-manager = GoogleSheetManager(credentials_file, sheet_name)
-
-# Call the insert_data method multiple times
-for record in all_data:
-    manager.insert_data(record)
+tm_api = TicketMasterAPI(base_url, api_key, credentials_file, sheet_name, 10)
+tm_api.get_all_data()
--- ticket_master.py
+++ ticket_master.py
@@ -1,14 +1,16 @@
 import requests
-
+from google_sheet_manager import GoogleSheetManager
 class TicketMasterAPI:
-    def __init__(self, base_url, api_key):
+    def __init__(self, base_url, api_key, credentials_file, sheet_name, page_limit=0):
         self.base_url = base_url
         self.api_key = api_key
         self.page_size = 20
         self.current_page = 0
         self.total_pages = 1
         self.total_elements = 0
-    
+        self.page_limit = page_limit
+        # Initialize the GoogleSheetManager object
+        self.gsh_manager = GoogleSheetManager(credentials_file, sheet_name)
     def get_data(self):
         url = f"{self.base_url}?apikey={self.api_key}&page={self.current_page}&size={self.page_size}"
         response = requests.get(url)
@@ -24,19 +26,30 @@ class TicketMasterAPI:
             record = {
                 'name': event['name'],
                 'date': event['dates']['start']['localDate'],
-                'venue': event['embedded']['venues'][0]['name'],
+                'venue': event['_embedded']['venues'][0]['name'],
                 # Add more fields as needed
             }
             records.append(record)
         return records
     
     def get_all_data(self):
-        all_records = []
+        # all_records = []
         while self.current_page < self.total_pages:
             print("--> getting data for page ", self.current_page, " of ", self.total_pages)
-            data = self.get_data()
-            records = self.parse_records(data)
-            all_records.extend(records)
-            self.current_page += 1
-        return all_records
-
+            if self.page_limit > 0 and self.current_page >= self.page_limit:
+              print("--> getting data for page complete as page limit is set: ", self.page_limit)
+              break
+            else:
+              data = self.get_data()
+              records = self.parse_records(data)
+              self.insert_data_into_google_sheet(records)
+              self.current_page += 1
+        # return all_records
+    def insert_data_into_google_sheet(self, records):
+        for record in records:            
+          # Extract values dynamically
+          result = []
+          for key, value in record.items():
+              result.append(value)
+          self.gsh_manager.insert_data(result)
+        
